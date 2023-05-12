85d2bab366a06037423fa5db4163981ab58b7962
Rishikesh-177 <rushikesh.rikibe17@gmail.com>
2023-05-12 10:46:47 +0530

    Game app

---
+++ src/TicTacToeGame/Square.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Square = () => {
+  return (
+    <div
+    style={{ border: "1px solid",
+     height: "100px",
+      width: "100%",
+     display: "flex",
+    JustifyContent: "center",
+alignItems: 'center'}} 
+    className='square'>
+      <h5>O</h5>
+    </div>
+  );
+};
+
+export default Square
